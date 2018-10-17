a273cee490b70c860884d7446948b63cb55cf4f0
Marco Acierno <marcoaciernoemail@gmail.com>
2018-10-17 07:41:33 -0700

    Add publish page

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.shortcuts import redirect, render
+from django.urls import path, reverse
+
+from providers import get_all_providers
 
 from .models import Metadata, Post
 
@@ -7,9 +11,6 @@ class MetadataInline(admin.StackedInline):
     model = Metadata
 
     def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
         return False
 
 
@@ -21,8 +22,53 @@ class PostAdmin(admin.ModelAdmin):
     fieldsets = (("Post", {"fields": ("title", "content", "status")}),)
     readonly_fields = ("status",)
 
-    def save_form(self, request, form, change):
-        if "_publish" in form.data:
-            form.instance.publish()
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path(
+                "<int:object_id>/publish/",
+                self.admin_site.admin_view(self.publish_recap),
+                name="publish_recap",
+            )
+        ]
+        return my_urls + urls
 
-        return super().save_form(request, form, change)
+    def response_change(self, request, obj):
+        if "_publish" in request.POST:
+            return redirect(reverse("admin:publish_recap", args=[obj.id]))
+
+        return super().response_change(request, obj)
+
+    def publish_recap(self, request, object_id):
+        post = Post.objects.get(id=object_id)
+
+        if request.method == "POST":
+            response = redirect(reverse("admin:posts_post_change", args=[object_id]))
+
+            if "_publish" in request.POST:
+                messages.success(request, "Post schedulated for publishing!")
+                post.publish()
+
+            return response
+
+        socials = []
+
+        for provider in get_all_providers():
+            if not provider.is_active:
+                continue
+
+            already_exists = Metadata.objects.filter(
+                post=post, provider_name=provider.NAMESPACE
+            ).exists()
+
+            socials.append(
+                {"name": provider.NAMESPACE, "already_exists": already_exists}
+            )
+
+        return render(
+            request,
+            "admin/posts/publish.html",
+            context=dict(
+                self.admin_site.each_context(request), post=post, socials=socials
+            ),
+        )
